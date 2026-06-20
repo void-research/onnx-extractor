59737a4655763d39c582a11ec9a6a4dba0e02ba2
PieInOblivion <14125393+PieInOblivion@users.noreply.github.com>
2026-06-21 04:15:57 +1000

    change to rerun-if-changed for proto

--- build.rs
+++ build.rs
@@ -1,17 +1,14 @@
 use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-    let generated_rs = out_dir.join("onnx.rs");
+    println!("cargo:rerun-if-changed=proto/onnx.proto");
 
-    if !generated_rs.exists() {
-        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
-        let repo_proto = manifest_dir.join("proto").join("onnx.proto");
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    let repo_proto = manifest_dir.join("proto").join("onnx.proto");
 
-        prost_build::Config::new()
-            .bytes(["."])
-            .compile_protos(&[&repo_proto], &[&manifest_dir])?;
-    }
+    prost_build::Config::new()
+        .bytes(["."])
+        .compile_protos(&[&repo_proto], &[&manifest_dir])?;
 
     Ok(())
 }
